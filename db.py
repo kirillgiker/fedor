a3fd39acaeb9a3ede3d73c6256f9d297a0af43d8
kirillgiker <98788848+kirillgiker@users.noreply.github.com>
2023-06-17 23:39:39 +0300

    Add files via upload

--- db.py
+++ db.py
@@ -10,3 +10,10 @@ class Database:
     def add_asks_bids(self, token, asks_bids):
         with self.connection:
             return self.connection.execute("UPDATE ARB SET orders = ? WHERE TOKEN_SYMBOL = ?", (asks_bids, token))
+    def examintation(self, token_symbol): #есть ли в таблице
+        with self.connection:
+            res = self.cursor.execute("SELECT * FROM ARB WHERE TOKEN_SYMBOL = ?", (token_symbol,)).fetchall()
+            return bool(len(res))
+    def delete_alldata_from_table(self):
+        with self.connection:
+            return self.connection.execute("DELETE FROM ARB")

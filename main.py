0093f643e9cc093301a6b1083b825cd9e132cc1a
kirillgiker <98788848+kirillgiker@users.noreply.github.com>
2023-06-17 23:21:01 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 #link on pairs https://www.bitget.com/ru/spot/BTCUSDT?type=spot
 #/html/body/div[4]/div/div/div/div[2]/div/div/div/div[3]/div/div[2] xpath for pairs
-import requests, json, sqlite3
-from bs4 import BeautifulSoup
-from bot.db import Database
+import requests, time, sqlite3, random
 from db import Database
 def Scrape_Pairs_Name():
     url = "https://api.bitget.com/api/spot/v1/public/currencies"
@@ -20,8 +18,25 @@ def Scrape_Orders(pair, proxy):
     return data
 #cтакан class="bj3ny71RBfDxEisyLBeI" class="DucjjuqM2nt_HpBQzwUj"
 #название токена class="SN7UY1iBNNtlyP7MCK7m"
-db = Database('vkbot.db')
+file = open("proxy.txt")
+proxiesList = [line for line in file]
+db = Database('main.db')
 global base, cur
-base = sqlite3.connect('vkbot.db')
+base = sqlite3.connect('main.db')
 db.__init__
-
+#db.add(1, "hqwe")
+db.delete_alldata_from_table()
+while True:
+    pairsName = Scrape_Pairs_Name()
+    for i in range(len(pairsName)):
+        try:
+            proxy = random.choice(proxiesList)
+            db.add(i, pairsName[i])
+            ordersAsksBids = Scrape_Orders(pairsName[i]+"USDT", proxy=proxy)
+            asks = str(ordersAsksBids["asks"])
+            bids = str(ordersAsksBids["bids"])
+            asks_bids = "{'asks': "+ asks + ", 'bids': " + bids +"}"
+            db.add_asks_bids(pairsName[i], asks_bids)
+        except:
+            continue
+    time.sleep(60*5)
